8327670532218e525b47a4364320a21e5ad7583e
leo.lopes <devleo.oliveira@gmail.com>
2023-09-01 16:56:11 -0300

    Apir com Prisma

---
+++ src/controllers/UserController.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from "express";
+
+export default {
+  async createUser(request: Request, response: Response) {
+    try {
+
+    }catch (error) {
+      return response.json({ message: error.message });
+    }
+  }
+}
